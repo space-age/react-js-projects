692d19eb9235b04130eca37295b420d701208962
Christopher Casillas <53498728+space-age@users.noreply.github.com>
2022-04-25 19:23:42 -0700

    Delete 02-reviews directory

--- 02-reviews/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Card.module.css";
-
-const Card = (props) => {
-  const classes = `${styles.review} ${props.className}`;
-
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
--- 02-reviews/src/components/UI/SectionContainer.js
+++
@@ -1,9 +0,0 @@
-import styles from "./SectionContainer.module.css";
-
-const SectionContainer = (props) => {
-  const classes = `${styles.container} ${props.className}`;
-
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default SectionContainer;
